5b682ea9cdc6f849589e39fe3a58a5e3dad6e3e9
Nick Millward <nicholasmillward@gmail.com>
2014-03-01 19:46:00 -0500

    Modified tabs for Inbox and Friends.  Restructured Fragments

--- gen/com/superlifesize/ribbit/R.java
+++ gen/com/superlifesize/ribbit/R.java
@@ -24,13 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_logout=0x7f080009;
-        public static final int action_settings=0x7f080008;
+        public static final int action_logout=0x7f080008;
+        public static final int action_settings=0x7f080007;
         public static final int emailField=0x7f080005;
         public static final int login_button=0x7f080002;
         public static final int pager=0x7f080004;
         public static final int passwordField=0x7f080001;
-        public static final int section_label=0x7f080007;
         public static final int signupButton=0x7f080006;
         public static final int signup_text=0x7f080003;
         public static final int usernameField=0x7f080000;
@@ -39,7 +38,8 @@ public final class R {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_signup=0x7f030002;
-        public static final int fragment_main_dummy=0x7f030003;
+        public static final int fragment_friends=0x7f030003;
+        public static final int fragment_inbox=0x7f030004;
     }
     public static final class menu {
         public static final int login=0x7f070000;
@@ -49,23 +49,24 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int email_address_hint=0x7f05000c;
-        public static final int hello_world=0x7f050006;
-        public static final int login_button_label=0x7f050009;
-        public static final int login_error_message=0x7f050010;
-        public static final int login_error_title=0x7f050011;
-        public static final int menu_logout_label=0x7f050012;
-        public static final int password_hint=0x7f050008;
-        public static final int signup_button_label=0x7f05000d;
-        public static final int signup_error_message=0x7f05000e;
-        public static final int signup_error_title=0x7f05000f;
-        public static final int signup_text=0x7f05000a;
-        public static final int title_activity_login=0x7f050005;
-        public static final int title_activity_signup=0x7f05000b;
+        public static final int email_address_hint=0x7f05000b;
+        public static final int empty_friends_label=0x7f050013;
+        public static final int empty_inbox_label=0x7f050012;
+        public static final int hello_world=0x7f050005;
+        public static final int login_button_label=0x7f050008;
+        public static final int login_error_message=0x7f05000f;
+        public static final int login_error_title=0x7f050010;
+        public static final int menu_logout_label=0x7f050011;
+        public static final int password_hint=0x7f050007;
+        public static final int signup_button_label=0x7f05000c;
+        public static final int signup_error_message=0x7f05000d;
+        public static final int signup_error_title=0x7f05000e;
+        public static final int signup_text=0x7f050009;
+        public static final int title_activity_login=0x7f050004;
+        public static final int title_activity_signup=0x7f05000a;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
-        public static final int username_hint=0x7f050007;
+        public static final int username_hint=0x7f050006;
     }
     public static final class style {
         /** 
