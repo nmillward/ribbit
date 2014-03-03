961908ca864f446ad54c17acb5b89837ad64a4a2
Nick Millward <nicholasmillward@gmail.com>
2014-03-02 21:35:48 -0500

    Added Edit Friends Screen. Selecting Friend Relationships with checkmarks. Store friend relationships in Parse. Display friends in Friends tab. Removing Friends.

---
+++ src/com/superlifesize/ribbit/ParseConstants.java
@@ -0,0 +1,10 @@
+package com.superlifesize.ribbit;
+
+public final class ParseConstants {
+
+	//Class Names
+	
+	//Field Names
+	public static final String KEY_USERNAME = "username";
+	public static final String KEY_FRIENDS_RELATION = "friendRelation";
+}
