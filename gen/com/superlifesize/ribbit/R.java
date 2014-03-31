2315edd302eeb8c462a0a0cf77dc06322df3c2de
Nick Millward <nicholasmillward@gmail.com>
2014-03-30 22:33:50 -0400

    Retrieve Message from Friends via Parse. View Image with Picasso. View Videos with Intent.

--- gen/com/superlifesize/ribbit/R.java
+++ gen/com/superlifesize/ribbit/R.java
@@ -22,20 +22,27 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int message_list_item_vertical_margin=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_action_camera=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_picture=0x7f020001;
+        public static final int ic_action_play_over_video=0x7f020002;
+        public static final int ic_action_send_now=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_camera=0x7f09000a;
-        public static final int action_edit_friends=0x7f090008;
-        public static final int action_logout=0x7f090009;
-        public static final int action_settings=0x7f090007;
+        public static final int action_camera=0x7f09000c;
+        public static final int action_edit_friends=0x7f09000a;
+        public static final int action_logout=0x7f09000b;
+        public static final int action_send=0x7f09000d;
         public static final int emailField=0x7f090005;
+        public static final int imageView=0x7f090007;
         public static final int login_button=0x7f090002;
+        public static final int messageIcon=0x7f090008;
         public static final int pager=0x7f090004;
         public static final int passwordField=0x7f090001;
+        public static final int senderLabel=0x7f090009;
         public static final int signupButton=0x7f090006;
         public static final int signup_text=0x7f090003;
         public static final int usernameField=0x7f090000;
@@ -44,14 +51,16 @@ public final class R {
         public static final int activity_edit_friends=0x7f030000;
         public static final int activity_login=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int activity_signup=0x7f030003;
-        public static final int fragment_friends=0x7f030004;
-        public static final int fragment_inbox=0x7f030005;
+        public static final int activity_recipients=0x7f030003;
+        public static final int activity_signup=0x7f030004;
+        public static final int activity_view_image=0x7f030005;
+        public static final int fragment_friends=0x7f030006;
+        public static final int fragment_inbox=0x7f030007;
+        public static final int message_item=0x7f030008;
     }
     public static final class menu {
-        public static final int login=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int signup=0x7f080002;
+        public static final int main=0x7f080000;
+        public static final int recipients=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -59,8 +68,10 @@ public final class R {
         public static final int email_address_hint=0x7f05000b;
         public static final int empty_friends_label=0x7f050013;
         public static final int empty_inbox_label=0x7f050012;
+        public static final int empty_recipients_list_msg=0x7f05001e;
         public static final int error_external_storage=0x7f050018;
         public static final int error_opening_file=0x7f05001b;
+        public static final int error_sending_message=0x7f050024;
         public static final int error_title=0x7f050016;
         public static final int file_size_too_large_error=0x7f05001c;
         public static final int general_error=0x7f050019;
@@ -71,14 +82,21 @@ public final class R {
         public static final int menu_camera_label=0x7f050017;
         public static final int menu_edit_friends_label=0x7f050014;
         public static final int menu_logout_label=0x7f050011;
+        public static final int menu_send_label=0x7f05001f;
         public static final int password_hint=0x7f050007;
+        public static final int select_file_error_message=0x7f050021;
+        public static final int select_file_error_message_title=0x7f050022;
+        public static final int send_message_error=0x7f050020;
         public static final int signup_button_label=0x7f05000c;
         public static final int signup_error_message=0x7f05000d;
         public static final int signup_error_title=0x7f05000e;
         public static final int signup_text=0x7f050009;
+        public static final int success_message=0x7f050023;
         public static final int title_activity_edit_friends=0x7f050015;
         public static final int title_activity_login=0x7f050004;
+        public static final int title_activity_recipients=0x7f05001d;
         public static final int title_activity_signup=0x7f05000a;
+        public static final int title_activity_view_image=0x7f050025;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
         public static final int username_hint=0x7f050006;
